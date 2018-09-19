c0d39a9ad8c984c34145ebb27263c27077274e21
LiBoss <1186593130@qq.com>
2018-09-20 02:36:28 +0800

    编写详情页

--- tianti-modules/tianti-module-gateway/src/main/webapp/static/js/config.js
+++ tianti-modules/tianti-module-gateway/src/main/webapp/static/js/config.js
@@ -19,10 +19,12 @@ window.root = window.ctx + "tianti/";
 window.static = window.ctx + "static/";
 window.notImgUrl = window.static + "images/img_null.png";
 
-window.server = window.location.protocol + "//" + window.location.host + "/";
+//window.server = window.location.protocol + "//" + window.location.host + "/";
+window.server = window.location.protocol + "//api.konglongri.cn:8080/";
 window.serverPath = window.server + tiantiInterfaceName + "/";
 //后台上传的图片地址
-window.serverUploadPath = window.server + tiantiAdminName + "/";
+//window.serverUploadPath = window.server + tiantiAdminName + "/";
+window.serverUploadPath = window.location.protocol + "//admin.konglongri.cn:8080/" + tiantiAdminName + "/";
 
 jc.require.url.push(window.static + "js/jquery.pagination.js");
 jc.require.url.push(window.static + "js/jquery.nicescroll.min.js");
